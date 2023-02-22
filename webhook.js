42f723635eedf010db14ee0c396f89b326c079fe
aj <66404074+ihyajb@users.noreply.github.com>
2023-02-22 18:55:38 -0500

    test

--- webhook.js
+++ webhook.js
@@ -7,12 +7,13 @@ module.exports.send = (
     payload,
     hideLinks,
     censorUsername,
+    hideCommits,
     color
 ) => {
     const repository = payload.repository.full_name;
     const commits = payload.commits;
     const size = commits.length;
-    const branch = payload.ref.split("/")[payload.ref.split("/").length - 1]; 
+    const branch = payload.ref.split("/")[payload.ref.split("/").length - 1];
     const url = payload.compare;
     censorUsername = censorUsername.toString();
 
@@ -25,19 +26,22 @@ module.exports.send = (
     let latest = commits[0];
     const count = size == 1 ? "Commit" : " Commits";
 
-      let AuthorEmbed = [
+    let AuthorEmbed = [
         `${latest.author.username} | ⚡ ${size} ${count}`,
         `https://avatars.githubusercontent.com/${latest.author.username}`,
         `https://github.com/${latest.author.username}`,
-      ]
+    ]
 
-      core.info(color);
     let embed = new discord.EmbedBuilder()
-        .setDescription(this.getChangeLog(payload, hideLinks, censorUsername))
-        .setColor(color)
-        .setAuthor({name: AuthorEmbed[0], iconURL: AuthorEmbed[1], url: AuthorEmbed[2]})
-        .setTitle(`📁 \`${repository}\`\n🌳 \`${branch}\``)
-    // .setTimestamp(Date.parse(latest.timestamp));
+    if (hideCommits) {
+        embed.setDescription(`${size} new ${count} to ${repository}:${branch}`)
+            .setColor(color)
+    } else {
+        embed.setDescription(this.getChangeLog(payload, hideLinks, censorUsername))
+            .setColor(color)
+            .setAuthor({ name: AuthorEmbed[0], iconURL: AuthorEmbed[1], url: AuthorEmbed[2] })
+            .setTitle(`📁 \`${repository}\`\n🌳 \`${branch}\``)
+    }
 
     if (!hideLinks) {
         embed.setURL(url);
@@ -65,7 +69,7 @@ module.exports.send = (
             })
             .catch((error) => reject(error));
     });
-}; 
+};
 
 module.exports.getChangeLog = (payload, hideLinks, censorUsername) => {
     core.info("Constructing Changelog...");
@@ -103,6 +107,42 @@ module.exports.getChangeLog = (payload, hideLinks, censorUsername) => {
         }
 
         // changelog += message.replaceAll('\n\n', '\n > ')
+
+        changelog += message
+    }
+
+    return changelog;
+};
+
+module.exports.getChangeLogHidden = (payload, hideLinks, censorUsername) => {
+    core.info("Constructing Changelog...");
+    const commits = payload.commits;
+    let changelog = "";
+
+    for (let i in commits) {
+        let commit = commits[i];
+        const firstUsername = commit.author.username[0];
+        const lastUsername = commit.author.username[commit.author.username.length - 1];
+        let username = commit.author.username;
+        if (censorUsername == 'true') {
+            username = `${firstUsername}...${lastUsername}`;
+        }
+        const repository = payload.repository;
+
+        if (commit.message.includes(repository.full_name) && hideLinks) {
+            const firstRepository = repository.full_name[0];
+            const lastRepository =
+                repository.full_name[repository.full_name.length - 1];
+            commit.message = commit.message.replaceAll(repository.full_name, `${firstRepository}...${lastRepository}`);
+        }
+
+        let sha = commit.id.substring(0, 6);
+        let message
+        if (commit.message.length > MAX_MESSAGE_LENGTH) {
+            message = commit.message.substring(0, MAX_MESSAGE_LENGTH) + "..."
+        } else {
+            message = `\`${sha}\` ${commit.message} by *@${username}*\n`
+        }
 
         changelog += message
     }
