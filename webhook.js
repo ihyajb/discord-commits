aa58d88e0952ef6ed5f2e0e5c83c3230b2bd1cc6
aj <66404074+ihyajb@users.noreply.github.com>
2024-02-04 03:10:43 -0500

    First Test Didnt work
    because it never does lol

--- webhook.js
+++ webhook.js
@@ -56,7 +56,7 @@ module.exports.getChangeLog = (commits) => {
 
         let message = commit.message.length > MAX_MESSAGE_LENGTH
             ? `${commit.message.slice(0, MAX_MESSAGE_LENGTH)}...`
-            : `[\`${sha}\`](${commit.url}) — ${title}${details ? `\n${details}` : ''}(${commit.author.username})`;
+            : `[\`${sha}\`](${commit.url}) — ${title}${details ? `\n${details}` : ''}(${commit.author.username})\n`;
 
         changelog += message;
     });
